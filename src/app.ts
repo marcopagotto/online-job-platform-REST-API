eecb817e4c06bd456b3fea69f38ecc9794ab84ad
marcopagotto <129309108+marcopagotto@users.noreply.github.com>
2024-05-02 20:08:47 +0200

    feat(app): async errors handler

--- src/app.ts
+++ src/app.ts
@@ -5,11 +5,16 @@ import compression from 'compression';
 import apicache from 'apicache';
 import cookieParser from 'cookie-parser';
 
+import 'express-async-errors';
+
+import { errorHandler } from './utils/errors';
+
 const app = express();
 
 app.use(bodyParser.json());
 app.use(compression());
 app.use(cookieParser());
 app.use(apicache.middleware('5 minutes'));
+app.use(errorHandler);
 
 export { app };
