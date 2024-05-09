21ae4c00b8209dc15865715c8cd4207fe064b933
marcopagotto <129309108+marcopagotto@users.noreply.github.com>
2024-05-09 07:40:29 +0200

    feat(app): routes

--- src/app.ts
+++ src/app.ts
@@ -10,6 +10,7 @@ import cookieParser from 'cookie-parser';
 import 'express-async-errors';
 
 import { errorHandler } from './utils/errors';
+import routes from './routes/index';
 
 const app = express();
 
@@ -18,6 +19,7 @@ checkPoolConnection();
 app.use(bodyParser.json());
 app.use(compression());
 app.use(cookieParser());
+app.use('/api', routes());
 app.use(apicache.middleware('5 minutes'));
 app.use(errorHandler);
 
