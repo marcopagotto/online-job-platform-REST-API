afeec08c5db3dfd68200b69cbe93f316fe645e4c
marcopagotto <129309108+marcopagotto@users.noreply.github.com>
2024-05-10 12:19:21 +0200

    test(users): POST user authentication

--- src/__test__/users.test.ts
+++ src/__test__/users.test.ts
@@ -1,6 +1,7 @@
 import { app } from '../app';
 import request from 'supertest';
 import { generateRandomEmail } from '../utils/utils';
+import { getUserByEmail, getUserBySessionToken } from '../models/users';
 
 describe('POST new user', () => {
   it('Should return 400 if all fields are missing', async () => {
@@ -75,5 +76,91 @@ describe('POST new user', () => {
     };
     const response = await request(app).post('/api/user').send(user);
     expect(response.status).toBe(201);
+  });
+});
+
+describe('POST user authentication', () => {
+  it('Should return 400 if any field is missing', async () => {
+    const user = {
+      email: 'example@email.com',
+    };
+    const response = await request(app).post('/api/user/login').send(user);
+    expect(response.status).toBe(400);
+  });
+
+  it('Should return 400 if no user with email provided is registered', async () => {
+    const user = {
+      email: 'example@email.com',
+      password: 'password',
+    };
+    const response = await request(app).post('/api/user/login').send(user);
+    expect(response.status).toBe(400);
+  });
+
+  it('Should return 403 if password is incorrect', async () => {
+    const user = {
+      email: 'example@email.com',
+      psw: 'wrong_password',
+    };
+    const response = await request(app).post('/api/user/login').send(user);
+    expect(response.status).toBe(403);
+  });
+
+  it('Should return 200 if request is sent correctly', async () => {
+    const user = {
+      email: 'example@email.com',
+      psw: 'password',
+    };
+    const response = await request(app).post('/api/user/login').send(user);
+    expect(response.status).toBe(200);
+  });
+
+  it("Should update user's previous session_token", async () => {
+    const userEmail = 'example@email.com';
+
+    const previousUser = await getUserByEmail(userEmail);
+    const previousSessionToken = previousUser[0].session_token;
+
+    const userAuthentication = {
+      email: 'example@email.com',
+      psw: 'password',
+    };
+
+    await request(app).post('/api/user/login').send(userAuthentication);
+
+    const currentUser = await getUserByEmail(userEmail);
+    const currentSessionToken = currentUser[0].session_token;
+
+    expect(previousSessionToken !== currentSessionToken);
+  });
+
+  it('User should be retrievable via session_token', async () => {
+    const userAuthentication = {
+      email: 'example@email.com',
+      psw: 'password',
+    };
+
+    const response = await request(app)
+      .post('/api/user/login')
+      .send(userAuthentication);
+
+    const sessionToken = response.body[0].session_token;
+
+    const user = await getUserBySessionToken(sessionToken);
+
+    expect(user[0].email === userAuthentication.email);
+  });
+
+  it('Should attach an AUTH-LOGIN cookie to user', async () => {
+    const userAuthentication = {
+      email: 'example@email.com',
+      psw: 'password',
+    };
+
+    const response = await request(app)
+      .post('/api/user/login')
+      .send(userAuthentication);
+
+    expect(response.header['set-cookie'][0].startsWith('AUTH-LOGIN'));
   });
 });
