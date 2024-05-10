13cbedaa4fe3b1395e239e9dbb509415ed71a9a2
marcopagotto <129309108+marcopagotto@users.noreply.github.com>
2024-05-10 08:47:20 +0200

    feat(schema): user authentication schema

---
+++ src/schemas/user-authentication.ts
@@ -0,0 +1,19 @@
+import { body } from 'express-validator';
+import { notExistingEmailValidator } from '../utils/custom-validators';
+
+export const userAuthenticationSchema = [
+  notExistingEmailValidator('email')
+    .isEmail()
+    .bail()
+    .withMessage('Field must be a valid email address.')
+    .isLength({ max: 255 })
+    .bail()
+    .withMessage('Email address must not exceed 255 characters.'),
+  body('psw')
+    .notEmpty()
+    .bail()
+    .withMessage('Field must not be empty.')
+    .isLength({ min: 1, max: 100 })
+    .bail()
+    .withMessage('Field must be between 1 and 100 characters long.'),
+];
