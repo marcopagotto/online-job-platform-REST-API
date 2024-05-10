c1498a279d26fbf68c45dc34e26dafc758387e8a
marcopagotto <129309108+marcopagotto@users.noreply.github.com>
2024-05-10 09:01:31 +0200

    feat(app): users rout updated

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
-import { createUser } from '../controllers/users';
+import { authenticateUser, createUser } from '../controllers/users';
 
 import { userRegistrationSchema } from '../schemas/user-registration';
+import { userAuthenticationSchema } from '../schemas/user-authentication';
 
 export default (router: Router) => {
   router.post('/user', userRegistrationSchema, createUser);
+  router.post('/user/login', userAuthenticationSchema, authenticateUser);
 };
