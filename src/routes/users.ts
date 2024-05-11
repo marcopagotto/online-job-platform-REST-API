b04a2530f822e2c20a8aeac7720f5fd960e8cec0
marcopagotto <129309108+marcopagotto@users.noreply.github.com>
2024-05-11 11:16:24 +0200

    feat(app): users rout updated

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 
-import { authenticateUser, createUser } from '../controllers/users';
+import { authenticateUser, createUser, deleteUser } from '../controllers/users';
 
 import { userRegistrationSchema } from '../schemas/user-registration';
 import { userAuthenticationSchema } from '../schemas/user-authentication';
+import { isAuthenticated } from '../middlewares/middlewares';
 
 export default (router: Router) => {
   router.post('/user', userRegistrationSchema, createUser);
   router.post('/user/login', userAuthenticationSchema, authenticateUser);
+  router.delete('/user', isAuthenticated, deleteUser);
 };
