5ea95ce2f78a3a52d444447405fe519125cc4aac
marcopagotto <129309108+marcopagotto@users.noreply.github.com>
2024-05-13 13:51:06 +0200

    feat(utils): generateRandomName

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -3,3 +3,8 @@ export const generateRandomEmail = (): string => {
   const domain = Math.random() > 0.5 ? 'example.com' : 'test.com';
   return `${username}@${domain}`;
 };
+
+export const generateRandomName = (): string => {
+  const name = Math.random().toString(36).substring(2, 7);
+  return name;
+};
