459a1f1dfd099c30da8989a06e5405e14b685838
marcopagotto <129309108+marcopagotto@users.noreply.github.com>
2024-05-13 14:11:38 +0200

    feat(comp): getCompanyByCompanyId controller

--- src/controllers/companies.ts
+++ src/controllers/companies.ts
@@ -3,7 +3,9 @@ import { validationResult, matchedData } from 'express-validator';
 import {
   registerCompany as newCompany,
   getCompanyByCompanyName,
+  getCompanyById
 } from '../models/companies';
+import { ApiError } from '../utils/errors';
 
 export const registerCompany = async (req: Request, res: Response) => {
   const errors = validationResult(req);
@@ -19,4 +21,22 @@ export const registerCompany = async (req: Request, res: Response) => {
   const company = await getCompanyByCompanyName(data.company_name);
 
   return res.status(201).json(company[0]);
+};
+
+export const getCompanyByCompanyId = async (req: Request, res: Response) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json(errors);
+  }
+
+  const data = matchedData(req);
+
+  const company = await getCompanyById(data.company_id);
+
+  if (!company[0][0]) {
+    throw new ApiError(`No company found with id: ${data.company_id}.`, 404);
+  }
+
+  return res.status(200).json(company[0]);
 };
