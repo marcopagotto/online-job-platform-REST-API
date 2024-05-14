94570c112a7af067e7f9c09975b339354dfc8b2e
marcopagotto <129309108+marcopagotto@users.noreply.github.com>
2024-05-14 18:09:57 +0200

    test(comp): delete company

--- src/__test__/companies.test.ts
+++ src/__test__/companies.test.ts
@@ -1,6 +1,7 @@
 import { app } from '../app';
 import request from 'supertest';
 import { generateRandomName } from '../utils/utils';
+import { getCompanyByCompanyName } from '../models/companies';
 
 describe('POST new company', () => {
   it("Should return 401 if user isn't logged in", async () => {
@@ -111,5 +112,82 @@ describe('GET company by company id', () => {
       .set('Cookie', cookie);
 
     expect(response.status).toBe(200);
+  });
+});
+
+describe('DELETE company', () => {
+  it("Should return 401 if user isn't logged in", async () => {
+    const response = await request(app).delete('/api/company/4');
+    expect(response.status).toBe(401);
+  });
+
+  it('Should return a 400 if user is correctly logged in but is trying to delete a non existing company', async () => {
+    const userLogin = {
+      email: 'example@email.com',
+      psw: 'password',
+    };
+
+    const loginResponse = await request(app)
+      .post('/api/user/login')
+      .send(userLogin);
+
+    const cookie = loginResponse.header['set-cookie'][0];
+
+    const response = await request(app)
+      .delete('/api/company/999999')
+      .set('Cookie', cookie);
+
+    expect(response.status).toBe(400);
+  });
+
+  it("Should return a 403 if user is correctly logged in but is trying to delete another user's company", async () => {
+    const userLogin = {
+      email: 'example@email.com',
+      psw: 'password',
+    };
+
+    const loginResponse = await request(app)
+      .post('/api/user/login')
+      .send(userLogin);
+
+    const cookie = loginResponse.header['set-cookie'][0];
+
+    const response = await request(app)
+      .delete('/api/company/3')
+      .set('Cookie', cookie);
+
+    expect(response.status).toBe(403);
+  });
+
+  it('Should return a 202 if user is correctly deleteing a company of theirs', async () => {
+    const userLogin = {
+      email: 'example@email.com',
+      psw: 'password',
+    };
+
+    const loginResponse = await request(app)
+      .post('/api/user/login')
+      .send(userLogin);
+
+    const cookie = loginResponse.header['set-cookie'][0];
+
+    const comapnyBody = {
+      company_name: generateRandomName(),
+    };
+
+    await request(app)
+      .post('/api/company')
+      .set('Cookie', cookie)
+      .send(comapnyBody);
+
+    const company = (
+      await getCompanyByCompanyName(comapnyBody.company_name)
+    )[0];
+    
+    const response = await request(app)
+      .delete(`/api/company/${company[0].company_id}`)
+      .set('Cookie', cookie);
+
+    expect(response.status).toBe(202);
   });
 });
