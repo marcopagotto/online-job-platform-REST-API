71b58d9009e8a8b413060c6316614c0691047109
marcopagotto <129309108+marcopagotto@users.noreply.github.com>
2024-05-16 19:11:09 +0200

    feat(schema): listing publishment schema

---
+++ src/schemas/listings/listing-publishment.ts
@@ -0,0 +1,44 @@
+import { body } from 'express-validator';
+import { existingCompanyByIdBodyValidator } from '../../utils/custom-validators';
+
+export const listingPublishmentSchema = [
+  existingCompanyByIdBodyValidator('employer_id'),
+  body('job_title')
+    .trim()
+    .notEmpty()
+    .bail()
+    .withMessage('Field must not be empty.')
+    .isLength({ min: 1, max: 100 })
+    .bail()
+    .withMessage('Field must be between 1 and 100 characters long.'),
+  body('description')
+    .trim()
+    .notEmpty()
+    .bail()
+    .withMessage('Field must not be empty.')
+    .isLength({ min: 1, max: 1000 })
+    .bail()
+    .withMessage('Field must be between 1 and 1000 characters long.'),
+  body('annual_salary')
+    .trim()
+    .notEmpty()
+    .bail()
+    .withMessage('Field must not be empty.')
+    .isInt()
+    .bail()
+    .withMessage('Value must be a number')
+    .isLength({ min: 1, max: 7 })
+    .bail()
+    .withMessage('Field must be between 1 and 7 characters long.'),
+  body('remote')
+    .trim()
+    .notEmpty()
+    .bail()
+    .withMessage('Field must not be empty.')
+    .isLength({ min: 1, max: 4 })
+    .bail()
+    .withMessage('Field must be between 1 and 4 characters long.')
+    .isBoolean()
+    .bail()
+    .withMessage('Value must be boolean (1,0, true or false).'),
+];
