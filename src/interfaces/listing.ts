b80ce17195247e30dc810a085f17ccb126854bda
marcopagotto <129309108+marcopagotto@users.noreply.github.com>
2024-05-16 19:13:55 +0200

    feat(interface): Listing

---
+++ src/interfaces/listing.ts
@@ -0,0 +1,9 @@
+export interface Listing {
+  job_listing_id: number;
+  employer_id: number;
+  job_title: string;
+  description: string;
+  posted_date: string;
+  annual_salary: boolean;
+  remote: string;
+}
