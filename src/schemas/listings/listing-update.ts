464d8bf321800b29dfeb92f731d5689a0ee749c6
marcopagotto <129309108+marcopagotto@users.noreply.github.com>
2024-05-18 14:38:37 +0200

    feat(schema): listing update schema

---
+++ src/schemas/listings/listing-update.ts
@@ -0,0 +1,48 @@
+import { existingListingByIdParamValidator } from '../../utils/custom-validators';
+import { body } from 'express-validator';
+
+export const listingUpdateSchema = [
+  existingListingByIdParamValidator('listing_id'),
+  body('job_title')
+    .optional()
+    .trim()
+    .notEmpty()
+    .bail()
+    .withMessage('Field must not be empty.')
+    .isLength({ min: 1, max: 100 })
+    .bail()
+    .withMessage('Field must be between 1 and 100 characters long.'),
+  body('description')
+    .optional()
+    .trim()
+    .notEmpty()
+    .bail()
+    .withMessage('Field must not be empty.')
+    .isLength({ min: 1, max: 1000 })
+    .bail()
+    .withMessage('Field must be between 1 and 1000 characters long.'),
+  body('annual_salary')
+    .optional()
+    .trim()
+    .notEmpty()
+    .bail()
+    .withMessage('Field must not be empty.')
+    .isInt()
+    .bail()
+    .withMessage('Value must be a number')
+    .isLength({ min: 1, max: 7 })
+    .bail()
+    .withMessage('Field must be between 1 and 7 characters long.'),
+  body('remote')
+    .optional()
+    .trim()
+    .notEmpty()
+    .bail()
+    .withMessage('Field must not be empty.')
+    .isLength({ min: 1, max: 4 })
+    .bail()
+    .withMessage('Field must be between 1 and 4 characters long.')
+    .isBoolean()
+    .bail()
+    .withMessage('Value must be boolean (1,0, true or false).'),
+];
