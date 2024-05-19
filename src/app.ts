500f1d04706c8a6b599339b546b40a96d5b4064a
marcopagotto <129309108+marcopagotto@users.noreply.github.com>
2024-05-19 12:20:58 +0200

    feat(db): initTablesIfNotExisting

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,5 @@
 import express from 'express';
 
-
 import bodyParser from 'body-parser';
 import compression from 'compression';
 import apicache from 'apicache';
@@ -10,9 +9,11 @@ import 'express-async-errors';
 
 import { errorHandler } from './utils/errors';
 import routes from './routes/index';
+import { initTablesIfNotExisting } from './db/connection';
 
 const app = express();
 
+initTablesIfNotExisting();
 
 app.use(bodyParser.json());
 app.use(compression());
--- src/db/connection.ts
+++ src/db/connection.ts
@@ -25,3 +25,58 @@ export const checkPoolConnection = async () =>
       throw new Error(err.code);
     });
 
+export const initTablesIfNotExisting = async () => {
+  try {
+    const [tables]: Array<any> = await pool.query('SHOW TABLES');
+
+    const tables_in_db = `Tables_in_${config.mysql.databaseName}`;
+    
+    const tableNames = tables.map((table: any) => table.tables_in_db);
+
+    if (!tableNames.includes('users')) {
+      await pool.query(`
+        CREATE TABLE users (
+          user_id INT PRIMARY KEY AUTO_INCREMENT,
+          forename VARCHAR(50) NOT NULL,
+          lastname VARCHAR(50) NOT NULL,
+          birthdate DATE NOT NULL,
+          sex VARCHAR(1) NOT NULL,
+          email VARCHAR(255) NOT NULL,
+          psw CHAR(64) NOT NULL,
+          salt CHAR(255) NOT NULL,
+          session_token CHAR(64)
+        )
+      `);
+    }
+
+    if (!tableNames.includes('company')) {
+      await pool.query(`
+        CREATE TABLE company (
+          company_id INT PRIMARY KEY AUTO_INCREMENT,
+          company_name VARCHAR(50) UNIQUE NOT NULL,
+          company_owner INT NOT NULL,
+          FOREIGN KEY (company_owner) REFERENCES users(user_id) ON DELETE CASCADE
+        )
+      `);
+    }
+
+    if (!tableNames.includes('listing')) {
+      await pool.query(`
+        CREATE TABLE listing (
+          listing_id INT PRIMARY KEY AUTO_INCREMENT,
+          employer_id INT NOT NULL,
+          job_title VARCHAR(100) NOT NULL,
+          description VARCHAR(1000) NOT NULL,
+          posted_date TIMESTAMP NOT NULL DEFAULT NOW(),
+          annual_salary INT NOT NULL,
+          remote VARCHAR(4) NOT NULL,
+          FOREIGN KEY (employer_id) REFERENCES company(company_id) ON DELETE CASCADE
+        )
+      `);
+    }
+
+    console.log('Tables have been initialized.');
+  } catch (error) {
+    console.error('Error creating tables:', error);
+  }
+};
